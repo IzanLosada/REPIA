85b44da8a50d8995c5e5245d935497fe8bea8b02
Quim <quim.perez21@gmail.com>
2026-03-25 20:26:01 +0100

    Actualitzem l'apartat de registrar-se

--- public/js/config.js
+++ public/js/config.js
@@ -2,7 +2,6 @@ firebase.initializeApp({
   apiKey: "AIzaSyDrCUIQ41uibOMscCMzv52zVFoSO2DBxZ8",
   authDomain: "repia-e9dfd.firebaseapp.com",
   projectId: "repia-e9dfd",
-  storageBucket: "repia-e9dfd.firebasestorage.app",
   messagingSenderId: "1019919912911",
   appId: "1:1019919912911:web:8ee411e3bb1bccd31f4cba",
   measurementId: "G-ZD5RQZL1TE"
